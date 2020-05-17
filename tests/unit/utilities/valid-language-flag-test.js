27c87a623f7d93b57827852cde94429f27d6fd82
Joseph D. Sumner <me@jds.dev>
2020-05-17 18:14:32 -0500

    Add unit test config + passing tests

--- tests/unit/utilities/valid-language-flag-test.js
+++ tests/unit/utilities/valid-language-flag-test.js
@@ -1,4 +1,20 @@
-/**
-  TODO: Unit tests: validation for `--language` flag option of `new` and `init`
-*/
+'use-strict';
 
+const isValidLanguageFlag = require('../../../lib/utilities/valid-language-flag');
+const expect = require('chai').expect;
+
+describe('lib/utilities/valid-language-flag', function () {
+  describe('Valid Language Flags', function () {
+    [
+      'en',
+      'en-gb',
+      'en-GB',
+      'EN',
+      'EN-gb',
+      'EN-GB',
+    ].forEach((languageFlag) => {
+      it(`'${languageFlag}' is a valid language code`, function () {
+        expect(isValidLanguageFlag(languageFlag)).to.be.ok;
+      });
+    });
+});

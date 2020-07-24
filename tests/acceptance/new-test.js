d3b0f8c40b9227fe5a97481d1282b0d7c55d59a1
Joseph D. Sumner <me@jds.dev>
2020-07-24 14:43:25 -0500

    Remove tests related to `flag used without spec` parser-bug

--- tests/acceptance/new-test.js
+++ tests/acceptance/new-test.js
@@ -164,30 +164,6 @@ describe('Acceptance: ember new', function () {
     expect(err.message).to.not.include('ember-cli command option');
   });
 
-  // Misuse: No specification + declared option
-  it('ember new with --lang flag but no specification fails with an error message; absorbs ember-cli option (declared)', async function () {
-    let err = await expect(ember(['new', 'foo', '--skip-npm', '--skip-bower', '--lang', '--skip-git'])).to.be.rejected;
-    expect(err.name).to.equal('SilentError');
-    expect(err.message).to.be.ok;
-    expect(err.message).to.include('An error with the `--lang` flag returned the following message:');
-    expect(err.message).to.include('Information about using the `--lang` flag:');
-    expect(err.message).to.not.include('set the app programming language');
-    expect(err.message).to.include('ember-cli command option');
-    expect(err.message).to.include('--skip-git');
-  });
-
-  // Misuse: No specification + hidden option
-  it('ember new with --lang flag but no specification fails with an error message; absorbs appended ember-cli option (hidden)', async function () {
-    let err = await expect(ember(['new', 'foo', '--skip-npm', '--skip-bower', '--skip-git', '--lang'])).to.be.rejected;
-    expect(err.name).to.equal('SilentError');
-    expect(err.message).to.be.ok;
-    expect(err.message).to.include('An error with the `--lang` flag returned the following message:');
-    expect(err.message).to.include('Information about using the `--lang` flag:');
-    expect(err.message).to.not.include('set the app programming language');
-    expect(err.message).to.include('ember-cli command option');
-    expect(err.message).to.include('--disable-analytics');
-  });
-
   // Misuse: Invalid Country Code
   it('ember new with --lang flag and invalid code fails with an error message', async function () {
     let err = await expect(ember(['new', 'foo', '--skip-npm', '--skip-bower', '--skip-git', '--lang=en-UK'])).to.be
